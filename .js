1f9421f1687db61092139f198e318e83a3415ca4
EmeraldNZ <ahyuancy@gmail.com>
2019-01-01 13:02:41 +1300

    Update .js

--- .js
+++ .js
@@ -4,8 +4,14 @@ function sumFibs(num) {
     while(arr[i] <= num){     
         i++;
         arr[i] = arr[i-1] + arr[i-2];
-        arr.concat(arr[i]);
+        arr.concat(arr[i]); // if replacing the .concat() with .push() -> Error -> as .push() will mutate the original array (or object)
     }
-   return arr.filter(x => (x % 2 === 1)).reduce((a,b) => a+b) - arr[arr.length-1];
+   return arr.filter(x => (x % 2 === 1)).reduce((a,b) => (a+b)) - arr[arr.length-1];
 }
 sumFibs(1000); // return 1785
+
+
+
+
+
+// Retrieved from 
