ad530adbd2cb7b77507ea8ffa94ec65279e58128
EmeraldNZ <ahyuancy@gmail.com>
2019-01-01 13:24:00 +1300

    Update .js

--- .js
+++ .js
@@ -1,3 +1,5 @@
+/**************  Solution #1    ************************************************************************************/
+
 function sumFibs(num) {
     let arr = [1,1];
     let i = 1;
@@ -12,6 +14,22 @@ sumFibs(1000); // return 1785
 
 
 
+/**************  Solution #2    ************************************************************************************/
+
+function sumFibs2(num) {
+    let previousNum = 0;
+    let currentNum = 1;
+    let sum = 0;
+    while(currentNum <= num){
+        if(currentNum % 2 !== 0){ // Use the modulo operand to check if the current number is odd or even
+            sum += currentNum;
+        }
+        // Complete the Fibonacci circle by rotating to get the next number and swap values after
+        currentNum += previousNum;
+        previousNum = currentNum - previousNum;       
+    }
+    return sum;
+}
 
 
-// Retrieved from 
+// Retrieved from https://guide.freecodecamp.org/certifications/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/sum-all-odd-fibonacci-numbers/
